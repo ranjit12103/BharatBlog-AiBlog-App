e8324f03b3c47fc7c71cc32c463e3df546ba6c1f
ranjt12103 <ranjitppatil10@gmail.com>
2025-12-30 00:24:12 +0530

    completed dynamic blog ui

--- Blog_main/urls.py
+++ Blog_main/urls.py
@@ -7,4 +7,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home')
-] +static(settings.MEDIA_URL, document=settings.MEDIA_ROOT)
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/models.py
+++ blogs/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     category_name = models.CharField(_("category name"),max_length=50,unique=True)
-    created_at = models.DateTimeField(_("Created at"), auto_now=True)
+    category_image = models.ImageField(_("Category Image"), upload_to='categories/', null=True, blank=True)
+    created_at = models.DateTimeField(_("Created at"), auto_now=True, null=True, blank=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
 
     class Meta:
@@ -23,7 +24,7 @@ class Blog(models.Model):
     title = models.CharField(_("Title"), max_length=100)
     slug = models.SlugField(_("Slug"), unique=True, blank=True)
     category = models.ForeignKey(Category, verbose_name=_("Categoty"), on_delete=models.CASCADE) 
-    author = models.ForeignKey(User, verbose_name=_(""), on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name=_("Author"), on_delete=models.CASCADE)
     featured_image = models.ImageField(_("Featured Image"), upload_to='uploads/%Y/%m/%d')
     short_description = models.TextField(_("Short Description"))
     blog_body = models.TextField(_("Blog Body"), max_length=2000)
